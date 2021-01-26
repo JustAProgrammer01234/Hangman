d006627f46ce4788765d94bc0d04229dacae09a9
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2021-01-26 08:40:26 +0800

    Update game.py

--- game.py
+++ game.py
@@ -5,32 +5,39 @@ with open('words.txt','r') as f:
 
 tries = 3
 word_answer = [letter for letter in word]
-word_answer.pop()
 wordlist = ['_' for char in word_answer]
+word_answer.pop()
 playing = True
+print('Welcome to Hangman!')
 while playing:
+    blank = ' '.join(wordlist)
     print('------------------------------')
     print('Tries:',tries) 
-    print(wordlist)
+    print(blank)
+    print('')
     word_input = input('Guess the letter: ')
     if word_input in word_answer and word_input not in wordlist:  
+        print('')
         print('You are right!')
+        print('')
         for char in enumerate(word_answer):
             if char[1] == word_input:        
                 wordlist[char[0]] = word_input        
     else:
+        print('')
         print('You are wrong')
+        print('')
         tries -= 1 
         
     if tries <= 0:
-        print(wordlist)
+        print(blank)
         print('')
         print('Game Over!')
         print('')
         print('The word was',word)
         playing = False
     if wordlist == word_answer:
-        print(wordlist)
+        print(blank)
         print('')
         print('You win!')
         print('')
